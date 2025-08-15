67723721222019d95be88aa4f583307006606e23
benyathippra15 <benyathip.eye@gmail.com>
2025-08-15 15:39:17 +0700

    update auto deplay

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	config "go-lab/config"
 	"go-lab/router"
 )
@@ -16,7 +13,7 @@ func main() {
 	// fmt.Printf("Server running at http://0.0.0.0:%s\n", port)
 
 	// bind ทุก interface (0.0.0.0) เพื่อเรียกจาก IP ภายนอกได้
-	log.Fatal(http.ListenAndServe("128.199.97.209", nil))
+	// log.Fatal(http.ListenAndServe("128.199.97.209", nil))
 
 	// fmt.Println("Server running at http://localhost:8080")
 	// log.Fatal(http.ListenAndServe(":8080", nil))
