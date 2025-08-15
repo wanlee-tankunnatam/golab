6bffc188dd19c6e7827f1c566f7ee63462547af8
benyathippra15 <benyathip.eye@gmail.com>
2025-08-15 13:41:15 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ func main() {
 	config.ConnectPostgres()
 	router.RegisterRoutes()
 
-	fmt.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := "80"
+	fmt.Printf("Server running at http://0.0.0.0:%s\n", port)
+
+	// bind ทุก interface (0.0.0.0) เพื่อเรียกจาก IP ภายนอกได้
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
+
+	// fmt.Println("Server running at http://localhost:8080")
+	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
