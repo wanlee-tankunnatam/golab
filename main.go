4402a8d81acbf74d034799193091c69815bd50b3
benyathippra15 <benyathip.eye@gmail.com>
2025-08-18 09:55:33 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	config.ConnectPostgres()
 	router.RegisterRoutes()
 
-	r := router.RegisterRoutes() // เอา router ที่ return มาจาก RegisterRoutes
+	router.RegisterRoutes() // เอา router ที่ return มาจาก RegisterRoutes
 
 	fmt.Println("Server running at IP http://128.199.97.209:80")
-	log.Fatal(http.ListenAndServe(":80", r)) // ✅ ใส่ router แทน nil
+	log.Fatal(http.ListenAndServe(":80", nil)) // ✅ ใส่ router แทน nil
 }
