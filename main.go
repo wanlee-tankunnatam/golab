f449ec80e521290248086417498319d3382f1a35
benyathippra15 <benyathip.eye@gmail.com>
2025-08-18 18:04:15 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,6 +14,6 @@ func main() {
 	// เอา router ที่ return จาก RegisterRoutes
 	r := router.RegisterRoutes() // ✅ assign router
 
-	fmt.Println("Server running at http://128.199.97.209:80")
+	fmt.Println("Server running at http ://128.199.97.209:80")
 	log.Fatal(http.ListenAndServe(":80", r)) // ✅ ใช้ router แทน nil
 }
